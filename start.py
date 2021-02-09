342ccbb08d1410d278d9ca7f2e17ccfe6f272f17
KamilH <herbetko.kamil@gmail.com>
2021-02-09 15:48:52 +0100

    Linear regression model with data plot

--- start.py
+++ start.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
 
-
 data = pd.read_csv("student-mat.csv", sep=";")
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
@@ -16,16 +15,27 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+best = 0
+"""
+for i in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
 
-with open("studentmodel.pickle", "wb")
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+"""
+pickle_in = open("studentmodel.pickle", "rb")
+
+linear = pickle.load(pickle_in)
 
 print("Co: ", linear.coef_)
 print("Intercept: ", linear.intercept_)
@@ -35,3 +45,9 @@ predictions = linear.predict(x_test)
 for i in range(len(predictions)):
     print(predictions[i], x_test[i], y_test[i])
 
+p = "G2"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
