5dfe014b6b42c5d30d46634fd6b4994131ee6f04
kamil <herbetko.kamil@gmail.com>
2021-04-23 21:03:05 +0200

    improving esthetics

--- FlappyBirdAI/main.py
+++ FlappyBirdAI/main.py
@@ -276,6 +276,7 @@ def run(config_path):
     winner = p.run(main, 50)
 
 
+
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-feedforward.txt")
