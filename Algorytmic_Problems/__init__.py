323da07906e1f138d80c3d1cb7e8f7ebd476474a
kamil <herbetko.kamil@gmail.com>
2021-05-05 22:48:55 +0200

    solution to a mixing color problem using segment tree

--- Algorytmic_Problems/__init__.py
+++ Algorytmic_Problems/__init__.py
@@ -2,7 +2,7 @@
 
 n, m = map(int, input().split())
 lista = [[0, 0, 0] for i in range(2*n)]
-answear = 0
+answer = 0
 
 
 
@@ -16,21 +16,23 @@ def update(v, tl, tr, l, r, new_val):
         update(v*2, tl, tm, l, min(r, tm), new_val)
         update(v*2 + 1, tm + 1, tr, max(l, tm + 1), r, new_val)
 
-def check(v, yellow, blue):
-    if lista[v][2] == 3:
-        return 0
-    if ((yellow or lista[v][0] == 1) and (blue or lista[v][1])):
-        return 1
-    else:
-        if v == 0:
+def check(v):
+    y = 0
+    b = 0
+    while v > 0:
+        if lista[v][2] == 3:
             return 0
-        else:
-            return check(v//2, yellow or lista[v][0] == 1, blue or lista[v][1] == 2)
+        if lista[v][1] == 2:
+            b = 1
+        if lista[v][0] == 1:
+            y = 1
+        v = v//2
+    return b*y
 
 for i in range(m):
     l, r, k = map(int, input().split())
     update(1, 1, n, l, r, k)
 for node in range(n, 2*n):
-    answear += check(node, False, False)
+    answer += check(node)
 
-print(answear)
+print(answer)
