d784959f294f2480289fc872c6e3ce4692c4d997
kamil <herbetko.kamil@gmail.com>
2021-05-16 22:07:53 +0200

    basic discord bot with sending messages

---
+++ Discord_Bots/bot.py
@@ -0,0 +1,64 @@
+import discord
+
+token = "ODM4MTMwOTgxOTYwNjc5NDU0.YI2oTQ.QTW6mDo87YdDJtEU4GTb5H8xfzo"
+id = 705131759658074112
+
+client = discord.Client()
+
+@client.event
+async def on_member_join(member):
+    for channel in member.server.channels:
+        if str(channel) == "general":
+            await client.send_message(f"""Welcome to the server {member.mention}!""")
+
+@client.event
+async def on_message(message):
+    id = client.get_guild(705131759658074112)
+    channels = ["ak"]
+    valid_users = ["Kamil Herbetko#7099"]
+
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hello") != -1:
+            await message.channel.send("Hi!")
+        elif message.content == "!users":
+            await message.channel.send(f"""# of members {id.member_count}""")
+client.run(token)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
